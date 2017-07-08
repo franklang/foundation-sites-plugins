0716bc97dec9540b367f003358886d0e72ca1ab4
franklang <lang.frank79@gmail.com>
2017-07-08 08:06:39 +0200

    [ADD] globalWidth and divider parameters + better code comments

--- js/foundation.widerOffcanvas.js
+++ js/foundation.widerOffcanvas.js
@@ -6,16 +6,23 @@
    * Extends "Foundation for Sites" v6.4.1 off-canvas plugin
    * to provide a wider than 250px-standard width off-canvas.
    *
-   * [WARNING] Tested for right-positioned off-canvas only so far.
+   * [WARNING] Working for right-positioned off-canvas only so far.
    *
    * Required third-party components:
    * - foundation.core.js
-   * - foundation.offcanvas.js
+   * - foundation.offcanvas.js (and it's own dependencies)
+   * - foundation.util.mediaQuery.js
    * - foundation.util.box.js
    *
    * Usage:
+   * - Make sure you have a DOM element which width points to the _settings.scss 
+   *   "$global-width" variable value.
+   *   Has to be an ID. Default: <div id="globalWidth"></div>.
+   *
    * $(document).widerOffcanvas({
-   *   triggeringBreakpoint: 'medium',
+   *   globalWidth: 'globalWidth',
+   *   divider: 2.33333,
+   *   triggeringBreakpoint: 'large',
    *   offCanvasID: '#offCanvasRight',
    *   pageID: '#page-wrapper'
    * });
@@ -24,9 +31,11 @@
   $.fn.widerOffcanvas = function(options){
 
     var defaults = {
-      triggeringBreakpoint: 'medium', // one of Foundation's breakpoint (small, medium, large, ...)
-      offCanvasID: '#offCanvasRight', // target offcanvas element ID
-      pageID: '#page-wrapper' // page ID
+      globalWidth: 'globalWidth', // ID of a DOM element which width's value points to the _settings.scss "$global-width" variable value.
+      triggeringBreakpoint: 'large', // One of Foundation's breakpoint (small, medium, large, ...). Triggers the plugin.
+      divider: 2.33333, // value to divide browser window width by. Sets your wider off-canvas width.
+      offCanvasID: '#offCanvasRight', // Target offcanvas element ID.
+      pageID: '#page-wrapper' // Page ID.
     }
 
     var settings = $.extend({}, defaults, options);
@@ -42,11 +51,10 @@
      * Set wider off-canvas specific width
      */
     function setWiderOffCanvasWidth() {
-      var globalWidth = Foundation.Box.GetDimensions(document.getElementById('globalWidth'));
+      var globalWidth = Foundation.Box.GetDimensions(document.getElementById(settings.globalWidth));
       var pageSideMarginWidth = (globalWidth.parentDims.width - globalWidth.width)/2;
-      var baseWiderOffCanvasWidth = globalWidth.parentDims.width/2.33333;
+      var baseWiderOffCanvasWidth = globalWidth.parentDims.width/settings.divider;
       var widerOffCanvasWidth = pageSideMarginWidth + baseWiderOffCanvasWidth;
-      console.log(pageSideMarginWidth + ', ' + baseWiderOffCanvasWidth + ', ' + widerOffCanvasWidth);
       
       $(settings.offCanvasID).css({
         'width': widerOffCanvasWidth,
@@ -60,7 +68,7 @@
      * Set page indent value (page slides to the opposite side regarding where off-canvas opens)
      */
     function setPageIndentValue() {
-      var globalWidth = Foundation.Box.GetDimensions(document.getElementById('globalWidth'));
+      var globalWidth = Foundation.Box.GetDimensions(document.getElementById(settings.globalWidth));
       var baseWiderOffCanvasWidth = globalWidth.parentDims.width/2.33333;
       
       $(settings.offCanvasID).on('opened.zf.offcanvas', function() {
@@ -77,22 +85,24 @@
     /**
      * On document ready...
      */
+    // [TODO] Add a .hidden class on off-canvas element; remove it on first triggering.
     if (Foundation.MediaQuery.atLeast(settings.triggeringBreakpoint)) {
       setWiderOffCanvasWidth();
       setPageIndentValue();
-    }    
+    }
 
     /**
      * On window resize...
      */
     $(window).on('resize', function() {
       destroyWiderOffCanvas();
-      
-      var globalWidth = Foundation.Box.GetDimensions(document.getElementById('globalWidth'));
+      $(settings.offCanvasID).foundation('close');
+
+      var globalWidth = Foundation.Box.GetDimensions(document.getElementById(settings.globalWidth));
 
       if (Foundation.MediaQuery.atLeast(settings.triggeringBreakpoint)) {
         setWiderOffCanvasWidth();
-        setPageIndentValue();
+        setPageIndentValue(); 
       }
     });
   };
